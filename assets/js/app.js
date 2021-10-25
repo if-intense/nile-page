947fb252545540be077dec4f9080e14734fb5050
インテンス <intense@intensunoiMac.local>
2021-10-25 12:52:57 +0900

    new link

---
+++ assets/js/app.js
@@ -0,0 +1,125 @@
+// English
+
+var vm = new Vue ({
+    el: '#app',
+    data: {
+        capabilities: [
+            {number: 1, text: 'Cut time and cost. Only costs are Exhibition and Negotiation chat fees.', image: 'assets/img/img014.png'},
+            {number: 2, text: 'Match with companies internationally.', image: 'assets/img/img017.png'},
+            {number: 3, text: 'Chat casually. Use stickers! Can check the progress of the chats.', image: 'assets/img/img016.png'},
+            {number: 4, text: 'Chat in multiple languages.', image: 'assets/img/flag.png'}
+        ]
+    }
+});
+
+var tr = new Vue ({
+    el: '#new',
+    data: {
+        accomplishments: [
+            {task: 'You would need to download apps or software to match online.*', solution: 'No need to purchase to PC or Smartphone. Use it directly. Quick and Easy!', image:'assets/img/img011.png' },
+            {task: 'You do not know how much people are viewing or how they are responding.*', solution: 'You can check how much one is viewing, or interested in your company.', image:'assets/img/img013.png' },
+            {task: 'You are hosting events and webinars, but the negotiation rate is underwhelming.*', solution: 'By using the stickers in the chat, you will have a smoother communication.', image:'assets/img/img012.png' },
+        ]
+    }
+})
+
+
+//French
+
+var vm = new Vue ({
+    el: '#app-fr',
+    data: {
+        capabilities: [
+            {number: 1, text: 'Faire des réductions de temps et de coûts. Les seuls coûts sont les frais de chats d\'exposition et de négociation.', image: '../assets/img/img014.png'},
+            {number: 2, text: 'Être mis en relation avec des sociétés à l’étranger.', image: '../assets/img/img017.png'},
+            {number: 3, text: 'Discuter de manière décontractée en utilisant des stickers !', image: '../assets/img/img016.png'},
+            {number: 4, text: 'Discuter dans plusieurs langues.', image: '../assets/img/flag.png'}
+        ]
+    }
+});
+
+var tr = new Vue ({
+    el: '#new-fr',
+    data: {
+        accomplishments: [
+            {task: 'Vous aurez besoin de télécharger des applications ou des logiciels pour être mis en relation en ligne.*', solution: 'Pas besoin de l\'acheter sur PC ou sur smartphone. Utilisez-le directement. Rapide et facile !', image:'../assets/img/img011.png' },
+            {task: 'Vous ne savez pas combien de personnes consultent votre page ou comment ils y réagissent.*', solution: 'Vous pouvez vérifier combien de personnes ont consulté votre page ou s\'intéressent à votre société.', image:'../assets/img/img013.png' },
+            {task: 'Vous organisez des événements et des webinaires, mais le taux de négociation est décevant.*', solution: 'En utilisant des stickers dans le chat, vos communications seront plus fluides.', image:'../assets/img/img012.png' },
+        ]
+    }
+})
+
+
+//Spanish
+
+var vm = new Vue ({
+    el: '#app-es',
+    data: {
+        capabilities: [
+            {number: 1, text: 'Reducir el tiempo y los costes. Los únicos costes son los de la exposición y los del chat de negociación.', image: '../assets/img/img014.png'},
+            {number: 2, text: 'Emparejar con empresas a nivel internacional.', image: '../assets/img/img017.png'},
+            {number: 3, text: 'Chatear de un modo informal. ¡Use stickers!', image: '../assets/img/img016.png'},
+            {number: 4, text: 'Chatear en varios idiomas.', image: '../assets/img/flag.png'}
+        ]
+    }
+});
+
+var tr = new Vue ({
+    el: '#new-es',
+    data: {
+        accomplishments: [
+            {task: 'Necesitaría descargar aplicaciones o software para emparejar en línea.*', solution: 'No es necesario comprarlo en PC o smartphone. Utilícelo directamente. ¡Rápido y fácil!', image:'../assets/img/img011.png' },
+            {task: 'No sabe cuánto está viendo la gente ni cómo responde.*', solution: 'Puede comprobar cuánto se ve o se interesa por su empresa.', image:'../assets/img/img013.png' },
+            {task: 'Usted organiza eventos y seminarios web, pero el índice de negociación es decepcionante.*', solution: 'Utilizando los stickers en el chat, tendrá una comunicación más fluida.', image:'../assets/img/img012.png' },
+        ]
+    }
+})
+
+//Chinese
+
+var vm = new Vue ({
+    el: '#app-ch',
+    data: {
+        capabilities: [
+            {number: 1, text: '节省时间和成本。开销仅仅只有展览费和磋商费。', image: '../assets/img/img014.png'},
+            {number: 2, text: '与国际公司进行匹配。', image: '../assets/img/img017.png'},
+            {number: 3, text: '闲聊。使用表情包！', image: '../assets/img/img016.png'},
+            {number: 4, text: '以多种语言进行聊天。', image: '../assets/img/flag.png'}
+        ]
+    }
+});
+
+var tr = new Vue ({
+    el: '#new-ch',
+    data: {
+        accomplishments: [
+            {task: '您需下载应用程序或软件才能进行在线匹配。*', solution: '无需在电脑端或智能手机上进行购买。直接使用。快捷方便！', image:'../assets/img/img011.png' },
+            {task: '您不知道有多少人正在观看或他们的后续反应。*', solution: '您可查看有多少人正在观看或对您的公司感兴趣。', image:'../assets/img/img013.png' },
+            {task: '您正在举办活动和网络研讨会，但磋商进度并不理想。*', solution: '在聊天中使用表情包，您的沟通将变得更加顺畅。', image:'../assets/img/img012.png' },
+        ]
+    }
+});
+
+//Japanese
+var vm = new Vue ({
+    el: '#app-jp',
+    data: {
+        capabilities: [
+            {number: 1, text: '出展にかかる時間やコストを大幅に削減。必要なのは月額費用と商談チャットチケット代だけ(商談企業はチケット代以外無料)。', image: '../assets/img/img014.png'},
+            {number: 2, text: '国際的なビジネスマッチングができます。', image: '../assets/img/img017.png'},
+            {number: 3, text: '商談用チャット機能あり。専用スタンプで、気軽にやり取りができる。やりとりの進捗・ステータス管理ができる。', image: '../assets/img/img016.png'},
+            {number: 4, text: '便利な多言語機能（英語・フランス語・スペイン語・中国語・日本語）。', image: '../assets/img/flag.png'}
+        ]
+    }
+});
+
+var tr = new Vue ({
+    el: '#new-jp',
+    data: {
+        accomplishments: [
+            {task: 'これまでのサービスでは、アプリやソフトをダウンロードしなければものばかり*', solution: 'NILEは、ダウンロードの必要なし！ネットがつながれば、直ぐに利用できます！', image:'../assets/img/img011.png' },
+            {task: 'どれくらいの企業がアクセスしたか、マッチング率などの情報を知ることができない*', solution: 'NILEは企業管理画面も充実！訪問者数分析はもちろん、チケット購入まで一元管理できます！', image:'../assets/img/img013.png' },
+            {task: '展示会イベントやウェビナーを実施してもなかなか成約率が上がらない*', solution: '商談用チャットのスタンプを使えば、より簡単・円滑ににコミュニケーションを取れます！', image:'../assets/img/img012.png' },
+        ]
+    }
+})
